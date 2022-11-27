7ac09f44a149cdfcdcbfda6c9d8f83ec16e09bc7
mcw1217 <mcw1217@naver.com>
2022-11-28 02:42:27 +0900

    Finish

--- Project/app001/diabetes2.py
+++ Project/app001/diabetes2.py
@@ -2,9 +2,10 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 import numpy as np
 from sklearn.model_selection import cross_validate
-from sklearn.ensemble import RandomForestClassifier
+from sklearn import tree,neighbors,svm, ensemble
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 import pickle
+import matplotlib.pyplot as plt
 
 def pro(data):
     tool = OneHotEncoder()
@@ -28,7 +29,7 @@ def data():
     return train_input, test_input, train_target, test_target
 
 def make_model():
-    model = RandomForestClassifier(n_jobs=-1)
+    model = ensemble.RandomForestClassifier(n_jobs=-1)
     return model
 
 def do_learn():
@@ -37,9 +38,29 @@ def do_learn():
     scores = cross_validate(model, train_input, train_target, return_train_score=True, n_jobs=-1)
 
     model.fit(train_input, train_target)
+    train_score = model.score(train_input, train_target)
     score = model.score(test_input, test_target)
-    print(score)
-    pickle.dump(model, open("model.t","wb"))
+    #성능 평균 및 최저점 분석
+    trainlist = list()
+    testlist= list()
+    crosstrain = list()
+    crosstest = list()
+    turnvalue= 1000
+    for i in range(turnvalue):
+        train_input, test_input, train_target, test_target = data()
+        model.fit(train_input,train_target)
+        scores = cross_validate(model, train_input, train_target, return_train_score=True, n_jobs=-1)
+        trainlist.append(model.score(train_input,train_target))
+        testlist.append(model.score(test_input,test_target))
+        crosstrain.append(np.mean(scores['train_score']))
+        crosstest.append(np.mean(scores['test_score']))
+        print(f"{i}번째 완료!")
+    print("score min:", "train:",np.min(trainlist) ,"test:", np.min(testlist))
+    print("score mean:", "train:",np.mean(trainlist) ,"test:", np.mean(testlist))
+    print("cross score min:", "train:",np.min(crosstrain) ,"test:", np.min(crosstest))
+    print("cross score mean:", "train:",np.mean(crosstrain) ,"test:", np.mean(crosstest))
+
+    pickle.dump(model, open("model2.t","wb"))
     
     
 do_learn()
